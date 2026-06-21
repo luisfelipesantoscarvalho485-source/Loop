c499e864b0dbe16a296e0edd4738e1b795f983cd
Miguelzinho10 <luisfelipesantoscarvalho485@gmail.com>
2026-06-21 15:37:06 -0300

    isoo

--- math.js
+++ math.js
@@ -1,17 +1,15 @@
-const button = document.querySelector(".botao")
+const button = document.querySelector(".botao");
 
-button.addEventListener("click" , sortear)
+button.addEventListener("click", sortear);
 
 function sortear() {
+  const valor1 = Math.ceil(document.querySelector(".primeiro").value);
+  const valor2 = Math.floor(document.querySelector(".max").value);
 
-    const valor1 = Math.ceil(document.querySelector(".primeiro").value)
-    const valor2 = Math.floor(document.querySelector(".max").value)
-    if(valor2>=valor1){
-        alert("O Primeiro Número Não Pode Ser Menor Que O Segundo" )
-    }
-    else{
-        const result = Math.floor(Math.random() * (valor2 - valor1 + 1)) +valor1
-        alert(result)
-        
-    }
+  if (valor2 > valor1) {
+    const result = Math.floor(Math.random() * (valor2 - valor1)) + valor1;
+    alert(result);
+  } else {
+    alert("O Valor Minino Tem Que Ser MENOR Que O valor Máximo");
+  }
 }
