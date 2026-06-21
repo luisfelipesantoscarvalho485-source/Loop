465f3caa49bd06b1bd738fe0697ccd305f22af45
Miguelzinho10 <luisfelipesantoscarvalho485@gmail.com>
2026-06-21 15:03:47 -0300

    Correct validation in sortear function
    Fix logic to prevent alert when first number is greater than or equal to the second.

--- math.js
+++ math.js
@@ -6,15 +6,11 @@ function sortear() {
 
     const valor1 = Math.ceil(document.querySelector(".primeiro").value)
     const valor2 = Math.floor(document.querySelector(".max").value)
-
     if(valor1 >= valor2){
         alert("O Primeiro Número Não Pode Ser Maior Que O Segundo")
-        return;
     }
     else{
-
     const result = Math.floor(Math.random() * (valor2 - valor1 + 1)) +valor1;
-    
     alert(result)
     }
 }
