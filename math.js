9d958dc39dadf2db1c71a43c8de3d3e4466ce714
Miguelzinho10 <luisfelipesantoscarvalho485@gmail.com>
2026-06-21 14:42:54 -0300

    Update math.js

--- math.js
+++ math.js
@@ -4,15 +4,10 @@ button.addEventListener("click" , sortear)
 
 function sortear() {
 
-    if(valor1 <= valor2){
-    alert("O Primeiro valor nao pode ser maior que o segundo")
-    }
-    else{
-
     const valor1 = Math.ceil(document.querySelector(".primeiro").value)
     const valor2 = Math.floor(document.querySelector(".max").value)
 
     const result = Math.floor(Math.random() * (valor2 - valor1 + 1)) +valor1;
     
-    alert(result)}
+    alert(result)
 }
